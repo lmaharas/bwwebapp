aa75006329851e3ad747cc0a8f73ee2671bd459c
Lynn Maharas <lynn@betaworks.com>
2014-01-22 22:33:05 -0500

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+      dustjs: {
+        compile: {
+          files: {
+            "libs/js/templates.js": ["templates/**/*.dust"]
+          }
+        }
+      },
+
+      watch: {
+        scripts: {
+          files: ['templates/**/*.dust'],
+          tasks: ['dustjs']
+        }
+      }
+    });
+
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-dustjs');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Default task.
+    grunt.registerTask('default', ['dustjs', 'watch']);
+
+};
