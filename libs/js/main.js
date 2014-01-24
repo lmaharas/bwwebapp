45c8f9d806b2f7dfddd98ac430d5786f67392230
Lynn Maharas <lynn@betaworks.com>
2014-01-24 14:25:39 -0500

    finally dust is working. templates are rendering based on nav selection

--- libs/js/main.js
+++ libs/js/main.js
@@ -1,20 +1,69 @@
 (function($, undefined) {
     "use strict";
 
+    var currentState = 'forecast';
+
+    function setURL() {
+        var hash = window.location.hash ? window.location.hash.substr(1) : currentState;
+        window.location.hash = hash;
+        setActiveNav(hash);
+        setBodyClass(hash);
+    }
+
+    function setActiveNav(path) {
+        $('.nav a').parent().removeClass('active');
+        $('.nav a[href=#' + path + ']').parent().addClass('active');
+    }
+
+    function setBodyClass(pageClass) {
+        $("body").addClass(pageClass);
+    }
+
+    function renderTemplate(path) {
+        $.getJSON("data.json", function(data){
+            dust.render(path, data, function(err, out) {
+                $("body").html(out);
+                setURL();
+            });
+        });
+    }
+
+    function bindEvents(data) {
+
+        var touchOrClickEvent = Modernizr.touch ? "touchstart" : "click";
+
+        // Toggle Temp Up/Down
+        $('.temps').on(touchOrClickEvent, function() {
+            $('.more').animate({ height: "toggle" });
+        });
+
+        // Set Hashchange trigger
+        $(window).on('hashchange', function(){
+            renderTemplate(window.location.hash.substr(1));
+        });
+
+    }
+
     function loadBody() {
 
         $.getJSON("data.json", function(data){
 
-            dust.render("body", data, function(err, out) {
+            dust.render("forecast", data, function(err, out) {
 
                 $("body").html(out);
 
+                bindEvents(data);
+                setURL();
+
             });
+
         });
+
     }
 
     $(document).ready(function() {
         loadBody();
     });
+
 
 })(jQuery);
