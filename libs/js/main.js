136050df4c588e05b4d6595be03cc2b0f3858db5
Lynn Maharas <lynn@betaworks.com>
2014-01-26 23:30:25 -0500

    addnote

--- libs/js/main.js
+++ libs/js/main.js
@@ -5,7 +5,7 @@
 
     function getURL() {
         var hash = window.location.hash ? window.location.hash.substr(1) : currentState;
-        if (hash == 'forecast' || hash == 'wear') {
+        if (hash == 'forecast' || hash == 'wear' || hash == 'addnote') {
             currentState = hash;
         } else {
             currentState = 'forecast';
@@ -21,7 +21,11 @@
 
     function setActiveNav(path) {
         $('.nav a').parent().removeClass('active');
-        $('.nav a[href=#' + path + ']').parent().addClass('active');
+        if (path !== 'addnote'){
+            $('.nav a[href=#' + path + ']').parent().addClass('active');
+        } else {
+            $('.nav a[href=#wear]').parent().addClass('active');
+        }
     }
 
     function setBodyClass(pageClass) {
@@ -38,9 +42,39 @@
         });
     }
 
-    // function storePicture() {
-    //     //localStorage.dataToStore =
-    // }
+    function storePic(picture) {
+        localStorage.dataToStore = picture;
+    }
+
+    function loadPic(file) {
+        var URL=window.URL|| window.webkitURL,
+            fileURL=URL.createObjectURL(file),
+            gifDiv = $('.wrapper').find('.gif');
+
+        if ( !gifDiv.length ) {
+
+            var img = new Image();
+
+            img.src = fileURL;
+            img.className = 'img';
+            $('.camera-btn').parent().prepend("<div class='gif'></div>");
+            $('.camera-btn').prev('.gif').prepend(img);
+
+        } else {
+
+            gifDiv.find('.img').attr('src', fileURL);
+
+        }
+
+        URL.revokeObjectURL(fileURL);
+    }
+
+    function storeNote() {
+        textNote = $('.add-page .note').html();
+        if(textNote !== '' || textNote === 'undefined') {
+            localStorage.dataToStore = textNote;
+        }
+    }
 
     function bindEvents() {
 
@@ -51,8 +85,37 @@
             $('.more').animate({ height: "toggle" });
         });
 
+        $('.camera-btn').on(touchOrClickEvent, function() {
+            $('#camera').click();
+            console.log('camera');
+
+            $('#camera').on('change', function(e) {
+                var picture=e.target.files[0];
+                console.log(picture);
+                loadPic(picture);
+            });
+        });
+
+        $('.store-note').on(touchOrClickEvent, function() {
+            storePic();
+            storeNote();
+        });
+
+        $('.add-btn').on(touchOrClickEvent, function(e) {
+            e.preventDefault();
+            $('.wear-page').fadeOut(400, function() {
+                renderTemplate('addnote');
+                history.pushState(null, null, "#addnote");
+            });
+        });
+
+        $('.back-btn').on(touchOrClickEvent, function(e) {
+            e.preventDefault();
+            window.history.back();
+        });
+
         // Set Hashchange trigger
-        $(window).on('hashchange', function(){
+        $(window).on('hashchange', function() {
             renderTemplate(window.location.hash.substr(1));
         });
 
